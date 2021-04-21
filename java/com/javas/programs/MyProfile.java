c77ea407d0303dd0d0a18306e95009efab146d0a
saradhi1991 <k.pardhasaradhi.mba@gmail.com>
2021-04-21 15:19:40 +0530

    maven program

---
+++ java/com/javas/programs/MyProfile.java
@@ -0,0 +1,38 @@
+package com.javas.programs;
+public class MyProfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Name ="Pardhasaradhi";
+		
+		
+		System.out.println("My Name is "+Name);
+		byte age = 28;
+		System.out.println("My age is "+ age );
+		
+		short birthyear =1991;
+		System.out.println("My birth year"+ birthyear);
+		
+		int cell =1234567890;
+		System.out.println("My Cell NUmber -"+cell);
+		
+		float salary=66590.45f;
+		System.out.println("My salary -"+ salary);
+		
+		double pf = 99000.78954566;
+		System.out.println("My PF-" +pf);
+		
+		char gender ='M';
+		System.out.println("Sex-"+gender);
+		
+		boolean working = true;
+		System.out.println("Now I am working-"+working);
+		
+		String email ="supermain123@gmail.com";
+		
+		System.out.println("My Email id is -"+email);
+					
+
+	}
+
+}
